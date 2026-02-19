27a1751966c3c92f1c0997ca77d15d6670198b37
Jdkdkdiriej8383 <denrudik2011@vk.com>
2026-02-19 14:44:12 +0500

    Окак

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 class Config:
-    BOT_TOKEN = "8435235057:AAFaO6pLMqiG-MuqgAIfUr9Rx4d6Ypfu3CQ"  # ← Замени на токен от @BotFather
+    BOT_TOKEN = "7702277533:AAFDboZy78gHjRdvYVW7qhC0eCvpdZnuCxY"  # ← Замени на токен от @BotFather
     PAYMENT_PROVIDER_TOKEN = ""  # Оставь пустым, если нет
     OWNER_ID = 1965081517  # ← Замени на свой Telegram ID
 
